4dbe9ddde7a02f52913a25c7928de652ee7fed69
arias99900 <aryaarias15@gmail.com>
2025-12-09 13:41:43 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/post', 'Post::index');
+$routes->get('post/create', 'Post::create');
+$routes->post('post/store', 'Post::store');
+$routes->get('post/delete/(:num)', 'Post::delete/$1');
+$routes->get('post/edit/(:num)', 'Post::edit/$1');
+$routes->post('post/update/(:num)', 'Post::update/$1');
